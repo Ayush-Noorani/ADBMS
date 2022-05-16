d5f9d57855b6fa005bffe0d9ac4433cbe0753c19
Ayush-Noorani <84242031+Ayush-Noorani@users.noreply.github.com>
2022-05-16 21:17:53 +0530

    Add files via upload

---
+++ includes/nav.php
@@ -0,0 +1,7 @@
+<div>
+    <?php echo "<h4>Welcome ".$_SESSION['user']. "</h4>" ?>
+        <div class="float-right">
+            <a href="includes/logout.php"><input type="submit" name="logout" value="Logout"
+            class="btn btn-lg" style="text-decoration: none; color: #000;"></a>
+        </div>
+</div>
